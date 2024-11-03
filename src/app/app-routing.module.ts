0188e33aeca90249c6a0476dbf67d822b16af1c7
AakashProductStack <aakashdeep@productstack.in>
2024-11-03 14:01:58 +0530

    Correct routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,25 +6,6 @@ import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   { path: 'login-signup', component: LoginSignupComponent },
-  // { path: 'dashboard', component: MainComponent, canActivate: [AuthGuard] },
-  { path: 'dashboard', component: MainComponent },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
-];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
-
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { MainComponent } from './main/main.component';
-import { LoginSignupComponent } from './login-signup/login-signup.component';
-import { AuthGuard } from './guards/auth.guard';
-
-const routes: Routes = [
-  { path: 'login-signup', component: LoginSignupComponent },
   { path: 'dashboard', component: MainComponent, canActivate: [AuthGuard] },
   // { path: 'dashboard', component: MainComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
