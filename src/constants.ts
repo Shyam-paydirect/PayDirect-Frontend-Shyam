e05d71389c7e440bf036a904c679a0c15c0f061d
EvilSpawn24 <sanjai.s@paydirectgo.com>
2025-02-20 16:55:11 +0530

    Stage to Yoda

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-const stagingApi = 'https://stage.paydirectgo.com:5000/api';
+const stagingApi = 'https://yoda.paydirectgo.com:5000/api';
 const liveApi = 'https://auth.paydirectgo.com:5000/api';
 const docsApi = 'https://dms.paydirectgo.com:3000/api';
 const testingApi = 'http://13.201.173.117:3000/api';
